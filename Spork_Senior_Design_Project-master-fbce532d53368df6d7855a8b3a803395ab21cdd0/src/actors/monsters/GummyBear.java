abb49f625107fc2705400e95486aeca84904bbef
Kurt Do <do006@knights.gannon.edu>
2019-11-10 14:33:22 -0500

    Create gummyBear monster class and sprite

---
+++ Spork_Senior_Design_Project-master-fbce532d53368df6d7855a8b3a803395ab21cdd0/src/actors/monsters/GummyBear.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actors.monsters;
+
+/**
+ *
+ * @author do006
+ */
+
+
+import java.awt.Toolkit;
+import java.util.concurrent.ThreadLocalRandom;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class GummyBear extends MeleeMonster {
+    
+    public GummyBear(double nx, double ny){
+        actorImg = new Image("/images/gummyBear.png",
+                             Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.1,
+                             Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.1,
+                             true, false);
+        imageView = new ImageView(actorImg);
+        
+        // This checking will have to be enhanced, it should not be able to be outside playable area.
+        if(nx > 0){
+            x = nx;
+        } else {
+            x = 0.0;
+        }
+        
+        if(ny > 0){
+            y = ny;
+        } else {
+            y = 0.0;
+        }
+        
+        top = getY();
+        bottom = getY() + actorImg.getHeight();
+        left = getX();
+        right = getX() + actorImg.getWidth();
+        
+        setStats();
+        
+        setMoveBounds();
+    }
+    
+    @Override
+    public void move(){
+        // Moves randomly
+        if(collision || hitBound){
+            changeDirection();
+            setCollision(false);
+        } else {
+            // Move the same direction
+        }
+        
+        super.move();
+        super.checkBounds();
+    }
+    
+    @Override
+    public void changeDirection(){
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 8); // Corresponds to directions in Direction enum in Actor.java
+            
+        switch(randomNum){
+            case 0:
+                setDy(-speed);
+                break;
+            case 1:
+                setDy(-speed);
+                setDx(speed);
+                break;
+            case 2:
+                setDx(speed);
+                break;
+            case 3:
+                setDy(speed);
+                setDx(speed);
+                break;
+            case 4:
+                setDy(speed);
+                break;
+            case 5:
+                setDy(speed);
+                setDx(-speed);
+                break;
+            case 6:
+                setDx(-speed);
+                break;
+            case 7:
+                setDy(-speed);
+                setDx(-speed);
+                break;
+        }
+    }
+    
+    @Override
+    protected void setStats(){
+        speed = 4;
+        attack = 1.0;
+        hp = 1.0;
+        
+        super.setStats();
+    }
+}
